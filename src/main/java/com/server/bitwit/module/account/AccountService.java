1fd82530a850c0161d79f0aafd9f3a0cae2f620e
mingi <tlsalsrl0617@gmail.com>
2021-07-09 14:04:24 +0900

    변경: createAccount가 accountId를 반환하도록 수정
    엔티티를 저장하는 경우 저장된 엔티티의 id를 반화하도록 합니다.

--- src/main/java/com/server/bitwit/module/account/AccountService.java
+++ src/main/java/com/server/bitwit/module/account/AccountService.java
@@ -1,9 +1,12 @@
 package com.server.bitwit.module.account;
 
 import com.server.bitwit.module.account.dto.AccountRequest;
+import com.server.bitwit.module.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,9 +16,14 @@ public class AccountService
     private final AccountRepository accountRepository;
     
     @Transactional
-    public void createAccount(AccountRequest request)
+    public Long createAccount(AccountRequest request)
     {
-        accountRepository.save(request.toAccount( ));
+        return accountRepository.save(request.toAccount( )).getId( );
+    }
+    
+    public Optional<Account> getAccount(Long accountId)
+    {
+        return accountRepository.findById(accountId);
     }
     
     public boolean existsById(Long accountId)
