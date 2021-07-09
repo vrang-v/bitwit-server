fe006bf775b62bfc69bc7d3935babe1d4c5e5efb
mingi <tlsalsrl0617@gmail.com>
2021-07-09 14:00:02 +0900

    변경: 추상화 된 BitwitErrorResponse를 반환하도록 수정

--- src/main/java/com/server/bitwit/infra/advice/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/server/bitwit/infra/advice/ExceptionHandlerControllerAdvice.java
@@ -1,7 +1,7 @@
 package com.server.bitwit.infra.advice;
 
+import com.server.bitwit.infra.exception.response.BitwitErrorResponse;
 import com.server.bitwit.infra.exception.BitwitException;
-import com.server.bitwit.infra.exception.ErrorResponse;
 import com.server.bitwit.infra.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,35 +9,35 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static com.server.bitwit.infra.exception.ErrorResponse.createErrorResponseEntity;
+import static com.server.bitwit.infra.exception.response.BitwitErrorResponse.createErrorResponseEntity;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionHandlerControllerAdvice
 {
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e)
+    public ResponseEntity<BitwitErrorResponse> handleNotFoundException(NotFoundException e)
     {
         log.error("handleNotFoundException", e);
         return createErrorResponseEntity(e);
     }
     
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e)
+    public ResponseEntity<BitwitErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e)
     {
         log.error("handleMethodArgumentNotValidException", e);
         return createErrorResponseEntity(new BitwitException(e.getMessage( )));
     }
     
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleBitwitException(BitwitException e)
+    public ResponseEntity<BitwitErrorResponse> handleBitwitException(BitwitException e)
     {
         log.error("handleBitwitException", e);
         return createErrorResponseEntity(e);
     }
     
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(Exception e)
+    public ResponseEntity<BitwitErrorResponse> handleException(Exception e)
     {
         log.error("handleException", e);
         return createErrorResponseEntity(new BitwitException(e.getMessage( )));
