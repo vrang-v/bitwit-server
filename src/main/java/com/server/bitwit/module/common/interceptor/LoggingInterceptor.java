166e1adc40e45c8adfb410aeb078c0acd8ea0b71
mingi <tlsalsrl0617@gmail.com>
2022-08-09 00:35:13 +0900

    변경: HTTP 요청, 응답 로그에 Query String을 표시합니다

--- src/main/java/com/server/bitwit/module/common/interceptor/LoggingInterceptor.java
+++ src/main/java/com/server/bitwit/module/common/interceptor/LoggingInterceptor.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.annotation.Nonnull;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,13 +13,23 @@ import javax.servlet.http.HttpServletResponse;
 public class LoggingInterceptor implements HandlerInterceptor {
     
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        log.info("[REQUEST]  [{}] [{}]", request.getRequestURI( ), request.getRemoteAddr( ));
+    public boolean preHandle(@Nonnull HttpServletRequest request, @Nonnull HttpServletResponse response, @Nonnull Object handler) {
+        var requestUri = getRequestUri(request);
+        log.info("[REQUEST]  [{}] [{}]", requestUri, request.getRemoteAddr( ));
         return true;
     }
     
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        log.info("[RESPONSE] [{}] [{}]", request.getRequestURI( ), response.getStatus( ));
+    public void afterCompletion(@Nonnull HttpServletRequest request, HttpServletResponse response, @Nonnull Object handler, Exception ex) {
+        var requestUri = getRequestUri(request);
+        log.info("[RESPONSE] [{}] [{}]", requestUri, response.getStatus( ));
+    }
+    
+    private static String getRequestUri(HttpServletRequest request) {
+        var requestUri = request.getRequestURI( );
+        if (request.getQueryString( ) != null) {
+            requestUri += "?" + request.getQueryString( );
+        }
+        return requestUri;
     }
 }
